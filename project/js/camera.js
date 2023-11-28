4f0e2deffe9272ad4b857c60cd751ec2a6eb80d8
Valentin_G <valentin.guillon93430@gmail.com>
2023-11-28 10:01:09 +0100

    downgrade: unfix enemy_chasing movement when camera moving

--- project/js/camera.js
+++ project/js/camera.js
@@ -25,10 +25,6 @@ export class Camera {
 
         for (const obj of My_Object.instances) {
             // if (obj.id == central_obj.id && obj instanceof Player_Object) { continue; }
-            if (obj.group == "enemy_chasing") {
-                obj.update_position(addX/2, addY/2);
-                continue;
-            }
             obj.update_position(addX, addY);
         }
     }
