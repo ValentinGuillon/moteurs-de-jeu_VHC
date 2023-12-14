2d23e7082787d0480187a34a1aa3e203a9e67eaa
Valentin_G <valentin.guillon93430@gmail.com>
2023-12-14 18:15:05 +0100

    update: remove text and change canvas ratio

--- project/js/script.js
+++ project/js/script.js
@@ -19,7 +19,7 @@ export const CNV = document.getElementById("myCanvas");
 export const CTX = CNV.getContext("2d");
 let temp = Math.max(Math.min(window.innerWidth, window.innerHeight), 100);
 CNV.width = temp
-CNV.height = temp * 0.7
+CNV.height = temp*0.7
 export const CNV10 = Math.floor(CNV.height * 0.1);
 
 CTX.imageSmoothingEnabled = false;
